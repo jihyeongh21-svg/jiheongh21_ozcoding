81f7dc172f35e441a4c2d34238deccc1ff47d31c
jihyeongh21-svg <jihyeongh21@gmail.com>
2026-01-30 01:52:33 +0900

    html,js 3일차 학습과제

--- html_css/html_day03/js/signup.js
+++ html_css/html_day03/js/signup.js
@@ -1,34 +1,44 @@
-//  제출 이벤트 받기 -> 이벤트 핸들링
-//  제출된 입력 값들을 참조
-//  입력값에 문제가 있는 경우 이를 감지
-//  가입 환영 인사를 제공
 const form = document.getElementById('form');
+
 form.addEventListener('submit', function (event) {
-    event.preventDefault()// 기존 기능을 차단 
+    // 1. 새로고침 막기
+    event.preventDefault(); 
 
-    let userId = event.target.id.value
-    let userPw = event.target.pw.value
-    let userPw2 = event.target.pwCheck.value
-    let userName = event.target.name.value
-    let userEmail = event.target.email.value
-    let userPhone = event.target.phone.value
-    let userGender = event.target.gender.value
-    let userPosition = event.target.position.value
-    let userIntro = event.target.intro.value
-
-    console.log(userId, userPw, userPw2, userName, userEmail, userPhone, userGender, userPosition, userIntro)
-
-    if(userId.length < 6){
-        alert("아이디가 너무 짧습니다 6자 이상 작성해 주세요 !")
-        return
-    }
-    if(userPw !== userPw2){
-        alert("비밀번호가 일치하지 않습니다 !")
-        return    
-    }
-    //  가입 환영 인사
-    document.body.innerHTML = ""
-    document.write(`<p>${userName}님 환영합니다.!!</p>`)
+    // 2. 입력값 가져오기
+    let userId = event.target.id.value;
+    let userPw = event.target.pw.value;
+    let userPw2 = event.target.pwCheck.value;
+    let userName = event.target.name.value;
+    let userEmail = event.target.email.value;
+    let userPhone = event.target.phone.value;
     
+    // 라디오 버튼(성별) 값 가져오기
+    let userGender = event.target.gender.value;
 
-}) 
+    // 3. 유효성 검사 (아이디 길이)
+    if(userId.length < 6){
+        alert("아이디가 너무 짧습니다. 6자 이상 작성해 주세요!");
+        return;
+    }
+    
+    // 4. 유효성 검사 (비밀번호 일치)
+    if(userPw !== userPw2){
+        alert("비밀번호가 일치하지 않습니다!");
+        return;    
+    }
+
+    // 5. 알림창에 보여줄 메시지 만들기
+    // \n 은 '줄바꿈' 기호입니다.
+    let message = `🎉 회원가입 성공! \n\n`;
+    message += `아이디: ${userId} \n`;
+    message += `이름: ${userName} \n`;
+    message += `이메일: ${userEmail} \n`;
+    message += `전화번호: ${userPhone} \n`;
+    message += `성별: ${userGender === 'male' ? '남자' : '여자'}`;
+
+    // 6. 알림창 띄우기 (여기서 사용자가 확인을 누를 때까지 멈춤)
+    alert(message);
+
+    // 7. 페이지 이동하기
+    window.location.href = "./admin.html"; 
+});
