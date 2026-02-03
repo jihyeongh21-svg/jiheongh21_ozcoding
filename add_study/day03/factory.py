11fd3125995424782c28b01e91a8e896ff02131b
jihyeongh21-svg <jihyeongh21@gmail.com>
2026-02-03 21:55:21 +0900

    팩토리 패턴

---
+++ add_study/day03/factory.py
@@ -0,0 +1,22 @@
+# 간단한 팩토리 메서드
+from abc import ABC, abstractmethod
+
+class PizzaStore:
+    def order_pizza(self, type):
+        pizza = self.create_pizza(type)
+        pizza.prepare()
+        pizza.bake()
+        pizza.cut()
+        pizza.box()
+        return pizza
+
+    @abstractmethod
+    def create_pizza(self, pizza_type):
+        pass
+
+class NYPizzaStore(PizzaStore):
+    def create_pizza(self, pizza_type):
+        if pizza_type == 'cheese':
+            return CheesePizza('NY Style Cheese Pizza')
+        
+        pass
