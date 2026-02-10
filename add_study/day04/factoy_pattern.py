93ccd5717092f7f7ae4589d550fcca26a7ec748d
jihyeongh21-svg <jihyeongh21@gmail.com>
2026-02-10 21:48:32 +0900

    팩토리 패턴 정리2일차

--- add_study/day04/factoy_pattern.py
+++ add_study/day04/factoy_pattern.py
@@ -15,16 +15,16 @@ class uploadserviecenter(ABC):
 
 class AwsS3(uploadserviecenter):
     def upload(self):
-        print("AWS S3에 파일을 업로드합니다.")
+        
 
     def create_thumnail(self):
-        print("AWS Lambda를 사용하여 썸네일을 생성합니다.")
+       
 
     def metadata(self):
-        print("AWS S3 객체 메타데이터를 설정합니다.")
+     
 
     def create_url(self):
-        print("CloudFront URL을 생성합니다.")
+      
 
 class Startup(uploadserviecenter):
     def upload(self):
