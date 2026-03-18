5270b69902ad01c27105b905f643d6c0e3e8acba
jihyeongh21-svg <jihyeongh21@gmail.com>
2026-03-18 16:34:31 +0900

    django 2일차

---
+++ django/day02/bookmark/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, get_object_or_404
+
+from bookmark.models import Bookmark
+
+# from django.http import Http404
+
+def bookmark_list(request):
+    bookmarks = Bookmark.objects.all()
+    context = {'bookmarks':bookmarks}
+    return render(request,'bookmark_list.html',context)
+def bookmark_detail(request,pk):
+    # try:
+    #     bookmark = Bookmark.objects.get(pk=pk)
+    # except:
+    #     raise Http404
+    bookmark = get_object_or_404(Bookmark,pk=pk)
+    context={'bookmark':bookmark}
+    return  render(request,'bookmark_detail.html',context)
