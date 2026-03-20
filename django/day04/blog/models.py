efb8f2087a9b4cd4d0f303270a7e668142063f9c
jihyeongh21-svg <jihyeongh21@gmail.com>
2026-03-20 13:40:43 +0900

    폴더명 수정

---
+++ django/day04/blog/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+# Create your models here.
+
+"""
+블로그 모델 
+1. 제목
+2. 본문
+3. 작성자
+4. 작성일자
+5. 수정일자
+6. 카테고리
+"""
+class Blog(models.Model):
+    CATEGORY_CHOICES = (
+        ('free','자유'),
+        ('travel','여행'),
+        ('study','공부'),
+        ('food','음식')
+    )
+
+    category = models.CharField('카테고리',max_length = 20,choices = CATEGORY_CHOICES)
+    # 작성자 추후 추가
+    title = models.CharField('제목',max_length=100)
+    content = models.TextField('본문')
+    created = models.DateTimeField('생성일자',auto_now_add=True)
+    updated = models.DateTimeField('수정일자',auto_now=True)
+
+    def __str__(self):
+        return f"[{self.get_category_display()}] {self.title}"
+
+    class Meta:
+        verbose_name = '블로그'
+        verbose_name_plural = '블로그 목록'
+
+
+
+
---
+++ django/day04/member/views.py
@@ -0,0 +1,45 @@
+from django.conf import settings
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login as django_login
+from django.urls import reverse
+
+
+# Create your views here.
+def signup(request):
+    username = request.POST.get('username')
+    password1 = request.POST.get('password1')
+    password2 = request.POST.get('password2')
+
+    # username 중복 체크
+    # 패스워드 규칙 준수 확인
+
+
+    form = UserCreationForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect(settings.LOGIN_URL)
+    """
+       아래와  같은 기능 POST 아닐 경우 None 
+    """
+    # if request.method == 'POST':
+    #     form = UserCreationForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         return redirect('/accounts/login/')
+    # else:
+    #     form = UserCreationForm()
+
+    context = {'form':form}
+
+    return render(request,'registration/signup.html',context)
+
+
+def login(request):
+    form = AuthenticationForm(request,request.POST or None)
+    if form.is_valid():
+        django_login(request, form.get_user())
+        return redirect(reverse('blog_list'))
+    context = {'form':form}
+    return render(request,'registration/login.html',context)
