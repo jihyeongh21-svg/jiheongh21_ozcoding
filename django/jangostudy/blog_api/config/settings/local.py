85ee946dcbd41061d28d74715136d95ce581f2fb
jihyeongh21-svg <jihyeongh21@gmail.com>
2026-03-31 10:54:55 +0900

    User 생성

---
+++ django/jangostudy/blog_api/config/settings/local.py
@@ -0,0 +1,16 @@
+import os
+
+from .base import *  # noqa
+
+DEBUG = os.getenv("DEBUG", True)
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
+    }
+}
